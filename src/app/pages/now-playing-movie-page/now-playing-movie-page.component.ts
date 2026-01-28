ef87f6984146563f15353a6125a0ba886cf57493
course-viktor-j12 <v.a.feshovych@gmail.com>
2026-01-28 11:04:37 +0200

    add loading-component to now-plaing

--- src/app/pages/now-playing-movie-page/now-playing-movie-page.component.ts
+++ src/app/pages/now-playing-movie-page/now-playing-movie-page.component.ts
@@ -4,13 +4,14 @@ import { Movie } from '../../interfaces/movie.interface';
 import { MovieListComponent } from '../../components/movie-list/movie-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { LoadingStateComponent } from '../../shared/loading-state/loading-state.component';
 
 @Component({
   selector: 'app-now-playing-movie-page',
   standalone: true,
-  imports: [MovieListComponent, HttpClientModule, CommonModule],
+  imports: [MovieListComponent, HttpClientModule, CommonModule, LoadingStateComponent],
   templateUrl: './now-playing-movie-page.component.html',
-  styleUrl: './now-playing-movie-page.component.scss'
+  styleUrls: ['./now-playing-movie-page.component.scss']
 })
 export class NowPlayingMoviePageComponent implements OnInit {
   constructor(private movieService: MovieService) { }
--- src/app/shared/loading-state/loading-state.component.ts
+++ src/app/shared/loading-state/loading-state.component.ts
@@ -2,8 +2,9 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-state',
-  template: '<p>{{ message }}</p>',
-  styleUrls: ['./loading-state.component.css']
+  standalone: true,
+  templateUrl: './loading-state.component.html',
+  styleUrls: ['./loading-state.component.scss']
 })
 export class LoadingStateComponent {
   @Input() message = 'Loading...';
