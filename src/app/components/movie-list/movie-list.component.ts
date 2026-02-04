5b18487e813bbf0b70e8e7e070260f32b20ddc56
course-viktor-j12 <v.a.feshovych@gmail.com>
2026-02-04 12:08:54 +0200

    add paginator to movie-list

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,35 +1,73 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { Movie } from '../../interfaces/movie.interface';
 import { CardModule } from 'primeng/card';
 import { RowLengthPipe } from '../../pipes/rowLength/row-length.pipe';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
-import { MovieService } from '../../services/movie/movie.service';
+import { PaginatorModule, PaginatorState } from 'primeng/paginator';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [CommonModule, MovieCardComponent, CardModule, RowLengthPipe],
+  imports: [
+    CommonModule,
+    MovieCardComponent,
+    CardModule,
+    RowLengthPipe,
+    PaginatorModule
+  ],
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent implements OnInit {
-  @Input() data!: Movie[];
-  public path: string = '';
+export class MovieListComponent implements OnInit, OnChanges {
+
+  @Input() data: Movie[] = [];
+
+  path = '';
+
+  readonly PAGE_SIZE = 3;
+
+  pagedData: Movie[] = [];
+
+  first = 0;
+  rows = this.PAGE_SIZE;
+  totalRecords = 0;
+  
+  get showPaginator(): boolean {
+  return this.totalRecords > this.PAGE_SIZE;
+}
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.url.subscribe((segments: UrlSegment[]) => {
-      if (segments.length > 1) {
-        this.path = segments[1].path;
-    }
+      this.path = segments[1]?.path ?? '';
     });
-}
+  }
 
-  public trackById(index: number, item: Movie): number {
-    return item.id;
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data']) {
+      this.totalRecords = this.data.length;
+      this.first = 0;
+      this.updatePage();
+    }
+  }
+
+  onPageChange(event: PaginatorState): void {
+    this.first = event.first ?? 0;
+    this.rows = event.rows ?? this.PAGE_SIZE;
+    this.updatePage();
+  }
+
+  private updatePage(): void {
+    this.pagedData = this.data.slice(
+      this.first,
+      this.first + this.rows
+    );
+  }
+
+  trackById(_: number, movie: Movie): number {
+    return movie.id;
   }
 }
-
-
